ed1fa41f88bcb9b1e57544264661e79fa4254bda
AWadhva <anujwadhva@gmail.com>
2022-04-28 09:25:31 +0530

    Factoring out some code into a new class ThalesReader

--- TTMain_Gate/MainTicketingRules.cs
+++ TTMain_Gate/MainTicketingRules.cs
@@ -23,55 +23,13 @@ namespace IFS2.Equipment.TicketingRules
             conf.rfPower = 2;
             cxnMonitor = new ThalesReaderConnectionMonitor(new V4ReaderApi(), this, "COM3:", 115200, conf);
             cxnMonitor.ReaderConnected += new Action<object>(ThalesReaderConnected);
-            cxnMonitor.ReaderDisconnected += new Action<object>(ReaderDisconnected);
-        }
+            cxnMonitor.ReaderDisconnected += new Action<object>(ReaderDisconnected);            
+        }        
         
         void ThalesReaderConnected(object obj)
         {
-            connectedReader = (ConnectedThalesReaderMin)obj;
-
-            bool bProductionSam = false; // ideally, it should have been in ISAMOnThalesReader
-            cchsSamMgr = new CCHSSAMManger(CSC_READER_TYPE.V4_READER, connectedReader.handle, bProductionSam);
-
-            ISAMConf conf = new ISAMConf();
-            conf.IsProductionSAM = false;
-            conf._readDeviceIDInCCHSSAM = true;
-            conf._signatureAtEachTransaction = true;
-            conf.hRw = connectedReader.handle;
-            conf.mCCHSSAMMgr = cchsSamMgr;
-            conf.slotId = 1;
-            conf.readerType = CSC_READER_TYPE.V4_READER;
-
-            isam = new ISAMOnThalesReader(conf);
-            bool bPresent;
-            bool bWorking;
-            isam.Initialize(out bPresent, out bWorking);
-            if (bPresent && bWorking)
-            {
-                ScenarioPolling sc1 = new ScenarioPolling();
-                sc1.ucAntenna = 1;
-                sc1.ucRepeatNumber = 1;
-                sc1.xCardType = (int)(CSC_TYPE.CARD_MIFARE1);
-
-                ScenarioPolling sc2 = new ScenarioPolling();
-                sc2.ucAntenna = 2;
-                sc2.ucRepeatNumber = 1;
-                sc2.xCardType = (int)(CSC_TYPE.CARD_MIFARE1);
-
-                V4ReaderMediaMonitor.V4ReaderAssembly rdr = new V4ReaderMediaMonitor.V4ReaderAssembly();
-                rdr.handle = connectedReader.handle;
-                rdr.isamSlots = new List<int> { conf.slotId };
-                rdr.rwTyp = CSC_READER_TYPE.V4_READER;
-                rdr.scenario1 = new List<ScenarioPolling> { sc1 };
-                rdr.scenario2 = new List<ScenarioPolling> { sc2 };
-                mediaMonitor = new V4ReaderMediaMonitor(this, rdr);
-                mediaMonitor.AddMediaProducedListener(this.MediaProduced);
-                mediaMonitor.AddMediaRemovedListener(this.MediaRemoved);
-
-                poller = new Poller(mediaMonitor, CSC_READER_TYPE.V4_READER, connectedReader.handle);
-                
-                poller.Start();
-            }
+            var connectedReader = (ConnectedThalesReaderMin)obj;
+            rdr1 = new ThalesReader(connectedReader, this, this.MediaProduced, this.MediaRemoved);
         }
 
         bool bSetAsEntry = true; // TODO: read it from context/configuration
@@ -80,38 +38,25 @@ namespace IFS2.Equipment.TicketingRules
         void MediaProduced(StatusCSCEx status)
         {
             if (bSetAsEntry)
-                curMediaTreatment = new CheckInTreatement(CSC_READER_TYPE.V4_READER, connectedReader.handle, null);
+                rdr1.curMediaTreatment = new CheckInTreatement(CSC_READER_TYPE.V4_READER, rdr1.connectedReader.handle, null);
             else
-                curMediaTreatment = new CheckOutTreatement(CSC_READER_TYPE.V4_READER, connectedReader.handle);
-            curMediaTreatment.Do(status);
+                rdr1.curMediaTreatment = new CheckOutTreatement(CSC_READER_TYPE.V4_READER, rdr1.connectedReader.handle);
+            rdr1.curMediaTreatment.Do(status);
         }
 
         void MediaRemoved(StatusCSCEx status)
         {
-            curMediaTreatment = null;
+            rdr1.curMediaTreatment = null;
         }
 
         void ReaderDisconnected(object obj)
         {
-            cchsSamMgr = null;
-            isam = null;
-            connectedReader = null;
-            mediaMonitor = null;
-            curMediaTreatment = null;
-            poller = null;
+            rdr1.poller.Stop();
+            rdr1 = null;
         }
 
         ReaderConnectionMonitor cxnMonitor; // one such object per r/w
-        CCHSSAMManger cchsSamMgr;    // one such object per r/w per its connected session. gets destroyed when r/w gets disconnected.
-        ISAMOnThalesReader isam; // one such object for every ISAM.
-        V4ReaderMediaMonitor mediaMonitor;  // one such object per V4 r/w per its connected session. gets destroyed when r/w gets disconnected.        
-        ConnectedThalesReaderMin connectedReader; // 1-per-reader per session
-
-        // It is reader-type independent (ALMOST). TODO:Make it completly reader-type independent.
-        IMediaTreatment curMediaTreatment = null; // 1-per-media appearance. Gets destroyed when media is removed
-
-        // It is reader-type independent (ALMOST). TODO:Make it completly reader-type independent.
-        Poller poller; // 1-per-reader per session
+        ThalesReader rdr1;
 
         public override int TreatMessageReceived(EventMessage eventMessage)
         {
@@ -135,12 +80,12 @@ namespace IFS2.Equipment.TicketingRules
 
                         if (messageId == suspendedOpTxnId)
                         {
-                            var monitor = mediaMonitor as V4ReaderMediaMonitor;
+                            var monitor = rdr1.mediaMonitor as V4ReaderMediaMonitor;
                             if (monitor != null)
                                 monitor.MakeCardReady();
 
-                            if (curMediaTreatment != null)
-                                curMediaTreatment.Resume();
+                            if (rdr1.curMediaTreatment != null)
+                                rdr1.curMediaTreatment.Resume();
                         }
                         
                         break;
@@ -155,10 +100,10 @@ namespace IFS2.Equipment.TicketingRules
         {
             bSetAsEntry = bCheckIn;
 
-            if (poller != null)
-                poller.Stop();
+            if (rdr1.poller != null)
+                rdr1.poller.Stop();
             
-            poller.Start();
+            rdr1.poller.Start();
         }
 
 #if WindowsCE
---
+++ TTMain_Gate/ThalesReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IFS2.Equipment.TicketingRules.SecurityModuleInitializer;
+using IFS2.Equipment.TicketingRules.MediaTreatment;
+using IFS2.Equipment.TicketingRules.CommonTT;
+using IFS2.Equipment.Common;
+using Common;
+
+namespace IFS2.Equipment.TicketingRules
+{
+    class ThalesReader
+    {
+        public CCHSSAMManger cchsSamMgr;
+        public ISAMOnThalesReader isam;
+        public V4ReaderMediaMonitor mediaMonitor;
+        public ConnectedThalesReaderMin connectedReader;
+
+        // It is reader-type independent (ALMOST). TODO:Make it completly reader-type independent.
+        public IMediaTreatment curMediaTreatment = null; // 1-per-media appearance. Gets destroyed when media is removed
+
+        // It is reader-type independent (ALMOST). TODO:Make it completly reader-type independent.
+        public Poller poller; // 1-per-reader per session
+
+        public ThalesReader(ConnectedThalesReaderMin rdrMin, ISyncContext syncContext, Action<StatusCSCEx> MediaProduced, Action<StatusCSCEx> MediaRemoved)
+        {
+            connectedReader = rdrMin;
+
+            bool bProductionSam = false; // ideally, it should have been in ISAMOnThalesReader
+            cchsSamMgr = new CCHSSAMManger(CSC_READER_TYPE.V4_READER, connectedReader.handle, bProductionSam);
+
+            ISAMConf conf = new ISAMConf();
+            conf.IsProductionSAM = false;
+            conf._readDeviceIDInCCHSSAM = true;
+            conf._signatureAtEachTransaction = true;
+            conf.hRw = connectedReader.handle;
+            conf.mCCHSSAMMgr = cchsSamMgr;
+            conf.slotId = 1;
+            conf.readerType = CSC_READER_TYPE.V4_READER;
+
+            isam = new ISAMOnThalesReader(conf);
+            bool bPresent;
+            bool bWorking;
+            isam.Initialize(out bPresent, out bWorking);
+            if (bPresent && bWorking)
+            {
+                ScenarioPolling sc1 = new ScenarioPolling();
+                sc1.ucAntenna = 1;
+                sc1.ucRepeatNumber = 1;
+                sc1.xCardType = (int)(CSC_TYPE.CARD_MIFARE1);
+
+                ScenarioPolling sc2 = new ScenarioPolling();
+                sc2.ucAntenna = 2;
+                sc2.ucRepeatNumber = 1;
+                sc2.xCardType = (int)(CSC_TYPE.CARD_MIFARE1);
+
+                V4ReaderMediaMonitor.V4ReaderAssembly rdr = new V4ReaderMediaMonitor.V4ReaderAssembly();
+                rdr.handle = connectedReader.handle;
+                rdr.isamSlots = new List<int> { conf.slotId };
+                rdr.rwTyp = CSC_READER_TYPE.V4_READER;
+                rdr.scenario1 = new List<ScenarioPolling> { sc1 };
+                rdr.scenario2 = new List<ScenarioPolling> { sc2 };
+                mediaMonitor = new V4ReaderMediaMonitor(syncContext, rdr);
+                mediaMonitor.AddMediaProducedListener(MediaProduced);
+                mediaMonitor.AddMediaRemovedListener(MediaRemoved);
+
+                poller = new Poller(mediaMonitor, CSC_READER_TYPE.V4_READER, connectedReader.handle);
+
+                poller.Start();
+            }
+        }
+    }
+}
